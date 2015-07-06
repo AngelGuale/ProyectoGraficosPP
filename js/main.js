875ce298072cd495e093817e0b39332284c36f1a
AngelGuale <adguale@espol.edu.ec>
2015-07-06 00:19:02 -0500

    ejes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var render, camara, escenario, figura, controls, territorio;
+var render, camara, escenario, figura, controls, territorio, axes;
 var ancho=window.innerWidth-10;
 var alto=window.innerHeight-50;
 var angulo=70;
@@ -270,7 +270,7 @@ function removerFiguras() {
    var obj, i;
 for ( i = escenario.children.length - 1; i >= 0 ; i -- ) {
     obj = escenario.children[ i ];
-    if ( obj !== territorio && obj !== camara) {
+    if ( obj !== territorio && obj !== camara && obj != axes) {
         escenario.remove(obj);
     }
 }
@@ -358,5 +358,41 @@ distancia=100;
 recorrido=distancia*tiempo;
 
 
-
  }
+
+ function buildAxes( length ) {
+        var axes = new THREE.Object3D();
+
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), 0xFF0000, false ) ); // +X
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), 0xFF0000, true) ); // -X
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), 0x00FF00, false ) ); // +Y
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), 0x00FF00, true ) ); // -Y
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), 0x0000FF, false ) ); // +Z
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), 0x0000FF, true ) ); // -Z
+
+        return axes;
+
+    }
+
+    function buildAxis( src, dst, colorHex, dashed ) {
+        var geom = new THREE.Geometry(),
+            mat; 
+
+        if(dashed) {
+            mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+        } else {
+            mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+        }
+
+        geom.vertices.push( src.clone() );
+        geom.vertices.push( dst.clone() );
+        geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+
+        var axis = new THREE.Line( geom, mat, THREE.LinePieces );
+
+        return axis;
+
+    }
+//To use enter the axis length
+axes = buildAxes( 1000 );
+    escenario.add( axes );
